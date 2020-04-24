373df805f8acf690f63a899961877c3d23290bfd
Murat0901 <menzilcim@gmail.com>
2020-04-25 00:51:04 +0300

    heroku branch

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
-import { ListGroup, Card } from "react-bootstrap";
 
 function index() {
   return (
@@ -10,11 +9,6 @@ function index() {
           <img src="https://avatars2.githubusercontent.com/u/47676205?s=460&u=3db26078eadd62852afdf3f952b04d238796761d&v=4"></img>
           <h1>Kişisel Websiteme Hoş Geldiniz</h1>
           <p>Blog sayfam için tıklayın</p>
-          <Link href="blog/">
-            <a>
-              <button className="my-btn">Blog Yazılarım</button>
-            </a>
-          </Link>
         </div>
       </section>
     </Layout>
